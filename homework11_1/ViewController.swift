5672433b7f66d5ab8f431ab006d8c7a960a3f879
MikeWu <MikeWu@mikewude-macbook-pro.local>
2016-05-23 00:11:45 +0800

    homework11_1

--- homework11_1/ViewController.swift
+++ homework11_1/ViewController.swift
@@ -1,25 +1,33 @@
-//
-//  ViewController.swift
-//  homework11_1
-//
-//  Created by MikeWu on 2016/5/22.
-//  Copyright © 2016年 Mike. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
-    override func viewDidLoad() {
+        let movieArray = [["name":"batMan1", "actor":"Mike"],
+                          ["name":"batMan2", "actor":"Robbin"],
+                          ["name":"batMan3", "actor":"Homer"]]
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieArray.count
+    }
 
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell_Id", forIndexPath: indexPath)
+        let dic = movieArray[indexPath.row]
+        
+        cell.textLabel?.text = dic["name"]
+        cell.detailTextLabel?.text = dic["actor"]
+        
+        return cell
+    }
 }
 
